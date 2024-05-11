2b18f6f8ef38a44470c89f3a490e13d1bc8e887b
vdmkkk <soloviov.vad@gmail.com>
2024-05-11 05:22:34 +0300

    adding stats

--- index.js
+++ index.js
@@ -28,54 +28,55 @@ app.get("/all-hosts", (req, res) => {
   });
 });
 
-app.get("/stats", (req, res) => {
-  exec("./system_monitor.sh", async (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return res.status(500).send({ error: `exec error: ${error.message}` });
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return res.status(500).send({ error: `stderr: ${stderr}` });
-    }
-    let lines = stdout.split("\n");
-    let [cpu1, cpu2] = [
-      parseInt(lines[0].split(" ").filter((el) => el != "")[3]),
-      parseInt(lines[0].split(" ").filter((el) => el != "")[5]),
-    ];
-    let [ram1, ram2] = [lines[1].split("/")[0], lines[1].split("/")[1]];
-    rams = [];
-    [ram1, ram2].forEach((el) => {
-      var el;
-      if (el.includes("Gi")) {
-        el = parseInt(parseFloat(el.slice(0, el.indexOf("Gi"))) * 1024);
-      } else {
-        el = parseInt(el.slice(0, el.indexOf("Mi")));
+const getStats = () => {
+  try {
+    exec("./system_monitor.sh", async (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
       }
-      rams.push(el);
-    });
-    let net = lines[2].slice(0, lines[2].indexOf("Mi"));
-    const unixTimestamp = Math.floor(Date.now() / 1000);
-    console.log(cpu1, cpu2);
-    console.log(rams);
-    console.log(net);
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+      }
+      let lines = stdout.split("\n");
+      let [cpu1, cpu2] = [
+        parseInt(lines[0].split(" ").filter((el) => el != "")[3]),
+        parseInt(lines[0].split(" ").filter((el) => el != "")[5]),
+      ];
+      let [ram1, ram2] = [lines[1].split("/")[0], lines[1].split("/")[1]];
+      rams = [];
+      [ram1, ram2].forEach((el) => {
+        var el;
+        if (el.includes("Gi")) {
+          el = parseInt(parseFloat(el.slice(0, el.indexOf("Gi"))) * 1024);
+        } else {
+          el = parseInt(el.slice(0, el.indexOf("Mi")));
+        }
+        rams.push(el);
+      });
+      let net = lines[2].slice(0, lines[2].indexOf("Mi"));
+      const unixTimestamp = Math.floor(Date.now() / 1000);
 
-    await db.query(
-      `INSERT INTO cpu (usr, sys, timestamp) values ($1, $2, $3) RETURNING *`,
-      [cpu1, cpu2, unixTimestamp]
-    );
-    await db.query(
-      `INSERT INTO ram (used, max, timestamp) values ($1, $2, $3) RETURNING *`,
-      [rams[0], rams[1], unixTimestamp]
-    );
-    await db.query(
-      `INSERT INTO network (used, timestamp) values ($1, $2) RETURNING *`,
-      [net, unixTimestamp]
-    );
-    // console.log(dbRes.rows);
-    res.send({ result: `OK` });
-  });
-});
+      await db.query(
+        `INSERT INTO cpu (usr, sys, timestamp) values ($1, $2, $3) RETURNING *`,
+        [cpu1, cpu2, unixTimestamp]
+      );
+      await db.query(
+        `INSERT INTO ram (used, max, timestamp) values ($1, $2, $3) RETURNING *`,
+        [rams[0], rams[1], unixTimestamp]
+      );
+      await db.query(
+        `INSERT INTO network (used, timestamp) values ($1, $2) RETURNING *`,
+        [net, unixTimestamp]
+      );
+      // console.log(dbRes.rows);
+    });
+  } catch (e) {
+    console.error("error during gathering stats: ", e);
+  }
+};
+
+const fiveMinutes = 5 * 60 * 1000;
+setInterval(getStats, fiveMinutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
