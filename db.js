5da6b678a2fdd84a2a5c0a92f1f7dc0408c84cad
vdmk <50838513+vdmkkk@users.noreply.github.com>
2024-05-11 05:40:16 +0300

    Create db.js
    its in gitignore

---
+++ db.js
@@ -0,0 +1,10 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "user",
+  password: "password",
+  host: "host",
+  port: "port",
+  database: "database",
+});
+
+module.exports = pool;
